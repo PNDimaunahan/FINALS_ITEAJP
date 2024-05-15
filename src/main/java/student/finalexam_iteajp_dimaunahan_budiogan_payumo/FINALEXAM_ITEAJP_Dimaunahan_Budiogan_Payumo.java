55fc15adb07e3cd2f50e546db7c15f2a392edc81
Paul Dimaunahan <paulnazarene.dimaunahan@letran.edu.ph>
2024-05-15 11:10:58 +0800

    Comit 1st

--- src/main/java/student/finalexam_iteajp_dimaunahan_budiogan_payumo/FINALEXAM_ITEAJP_Dimaunahan_Budiogan_Payumo.java
+++ src/main/java/student/finalexam_iteajp_dimaunahan_budiogan_payumo/FINALEXAM_ITEAJP_Dimaunahan_Budiogan_Payumo.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package student.finalexam_iteajp_dimaunahan_budiogan_payumo;
 
-/**
- *
- * @author Paul Dimaunahan
- */
+
 public class FINALEXAM_ITEAJP_Dimaunahan_Budiogan_Payumo {
 
     public static void main(String[] args) {
